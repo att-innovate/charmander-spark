0d887f2b56a362f6637517739f50a23aab88d278
Marcel Neuhausler <neuhausler@gmail.com>
2014-12-18 12:30:00 -0800

    reformatting and updated header

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -1,14 +1,34 @@
-// ./spark-submit --master yourMasterURL --class QueueStream .jar appName field
-// i.e. ./spark-submit --master spark://Theodoras-MacBook-Pro.local:7077 --class QueueStream /Users/theodorachu/ipynb/Charmander/GetDataInSpark/target/scala-2.10/get-streaming-data_2.10-1.0.1.jar sim-memory-light memory 
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 AT&T
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-import org.json4s.jackson.JsonMethods
+
+// ../spark/bin/spark-submit --class "SimpleApp --master local[*]  target/scala-2.10/simple-project_2.10-1.0.jar
 
 import scala.collection.mutable.SynchronizedQueue
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
+import org.json4s.jackson.JsonMethods
 import java.io._
 import java.net._
 
@@ -38,7 +58,7 @@ object SimpleApp {
     var out = new PrintWriter(socket.getOutputStream(), true)
     var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
     out.println("*2\r\n$3\r\nGET\r\n$" + key.length.toString + "\r\n" + key + "\r\n")
-    if(in.readLine().charAt(1) == '-') //Redis responses with $-1 if no value found
+    if (in.readLine().charAt(1) == '-') //Redis responses with $-1 if no value found
       return ""
     else
       return in.readLine()
@@ -75,8 +95,7 @@ object SimpleApp {
 
     // Create the QueueInputDStream and use it do some processing
     val inputStream = ssc.queueStream(rddQueue)
-    inputStream.foreachRDD( rdd =>
-    {
+    inputStream.foreachRDD(rdd => {
       println("Max: " + globalMax)
       val memoryusage = rdd.map(p => MemoryUsage(BigDecimal(p(0).asInstanceOf[BigInt]), BigDecimal(p(2).asInstanceOf[BigInt])))
       memoryusage.registerAsTable("memoryusage")
